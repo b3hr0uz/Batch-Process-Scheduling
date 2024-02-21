dcabfea10938a67dd8d3e78d05349b214d7a2b42
b3hr0uz <b3h@me.com>
2024-02-20 20:58:23 -0800

    Initial Commit

---
+++ Batch-Process-Scheduling/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Batch-Process-Scheduling
+//
+//  Created by Behrouz Barati on 2/20/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
